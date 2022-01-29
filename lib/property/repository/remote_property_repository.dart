ccc7dcaf95af31ba784a0bbfa2473c7d02c29def
ayham <ayham.orfali@metos.at>
2022-01-29 17:42:45 +0100

    Injected the property repository

--- lib/property/repository/remote_property_repository.dart
+++ lib/property/repository/remote_property_repository.dart
@@ -3,11 +3,13 @@ import 'dart:async';
 import 'package:funda_assignment/common/data/base_repository.dart';
 import 'package:funda_assignment/property/model/property.dart';
 import 'package:funda_assignment/property/service/property_service.dart';
+import 'package:injectable/injectable.dart';
 
 abstract class PropertyRepository implements BaseRepository {
   Future<Property> getPropertyById(String propertyId);
 }
 
+@Injectable(as: PropertyRepository)
 class RemotePropertyRepository implements PropertyRepository {
   final PropertyService _propertyService;
   RemotePropertyRepository(this._propertyService);
